c9e3e207a53ea525b41cecc4ed4270740d53231f
yujaemin <youjmen15@gmail.com>
2020-12-17 16:08:03 +0900

    [ADD] legacy xml

---
+++ Android/app/src/main/java/com/example/muscleup/ui/bodychangelist/BodyChangeListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.muscleup.ui.bodychangelist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.muscleup.R
+
+class BodyChangeListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_body_change_list)
+    }
+}
---
+++ Android/app/src/main/java/com/example/muscleup/ui/bodychangelist/BodyChangeModifyFragment.kt
@@ -0,0 +1,20 @@
+package com.example.muscleup.ui.bodychangelist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.muscleup.R
+
+
+class BodyChangeModifyFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_body_change_modify, container, false)
+    }
+
+
+}
